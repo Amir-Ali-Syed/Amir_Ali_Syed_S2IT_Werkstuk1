60014df61a2c77ce1fc7e9e8f085f87d60016a11
Amir_Ali_Syed <amir.ali.syed@student.ehb.be>
2018-04-30 15:06:08 +0200

    Initial Commit

---
+++ Amir_Ali_Syed_S2IT_Werkstuk1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Amir_Ali_Syed_S2IT_Werkstuk1
+//
+//  Created by Amir Ali Syed on 30/04/2018.
+//  Copyright © 2018 Amir Ali Syed. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
